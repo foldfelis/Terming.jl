87155b4b32b830e294c749a66ad5a1d9e1343049
JingYu Ning <foldfelis@gmail.com>
2020-07-23 23:23:04 +0800

    refactor

--- src/terminal.jl
+++ src/terminal.jl
@@ -50,7 +50,8 @@ export
 const CSI = REPL.Terminals.CSI
 
 displaysize(t=term) = REPL.Terminals.displaysize(t)
-raw!(enable::Bool, t=term) = REPL.Terminals.raw!(t, enable)
+raw!(t::REPL.Terminals.UnixTerminal, enable::Bool) = REPL.Terminals.raw!(t, enable)
+raw!(enable::Bool) = raw!(term, enable)
 
 cmove_up(stream::IO, n::Int) = Base.write(stream, "$(CSI)$(n)A")
 cmove_up(n::Int) = cmove_up(out_stream, n)
